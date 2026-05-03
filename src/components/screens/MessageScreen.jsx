6220f223063e9fb97b1cf2961e5ac605e48857f1
tinkarkrishna2-ux <tinkarkrishna2@gmail.com>
2026-05-03 17:53:01 +0530

    Update MessageScreen.jsx

--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -27,13 +27,13 @@ export default function MessageScreen({ onNext }) {
                     className=" h-auto max-w-xl bg-gradient-to-br from-pink-200 via-pink-100 to-pink-50 rounded-2xl shadow-lg p-4 md:p-6 text-center"
                 >
                     <p className="text-[#301733] text-base md:text-lg leading-relaxed overflow-y-auto max-h-[400px] pr-2">
-                        Happy Birthday, Cutiepie! You deserve all the happiness, love, and smiles in the world today and always.
+                        Happy Birthday, Bbyy! You deserve all the happiness, love, and smiles in the world today and always.
                         You have this special way of making everything around you brighter, your smile, your kindness, and the way
                         you make people feel truly cared for. I hope your day is filled with laughter, surprises, and moments that
                         make your heart happy. You’re truly one of a kind, and I just want you to know how special you are.
                         Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness,
                         success, and all the sweet things life has to offer. 💗
-                        Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness,
+                        Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness, loveeee youuuu sooo muchhh bbyy 💗 
                     </p>
                 </motion.div>
             </div>
