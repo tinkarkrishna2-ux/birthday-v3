db46b0135001e8718bc25dbb1e1b1679e3fdb9af
tinkarkrishna2-ux <tinkarkrishna2@gmail.com>
2026-05-03 17:54:04 +0530

    Update IntroScreen.jsx

--- src/components/screens/IntroScreen.jsx
+++ src/components/screens/IntroScreen.jsx
@@ -18,7 +18,7 @@ export default function IntroScreen({ onNext }) {
                         style={{
                             filter: "drop-shadow(0 0 20px rgba(255,105,180,0.4))",
                         }}>
-                        A Cutiepie was born today, 20 years ago!
+                        An angel was born today, 15 years ago!
                     </h1>
                     <p className="mt-4 text-xl text-pink-200">Yes, it’s YOU! A little surprise awaits...</p>
                 </div>
